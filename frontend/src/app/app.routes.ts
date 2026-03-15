39d3bcf5a83a70017822bbb44b25b2ee52ba04c0
Dhruv Dattani <dhruvd22@gmail.com>
2026-03-15 09:48:03 -0400

    Phase 2: Added routing and component scaffolding

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { TripsComponent } from './components/trips/trips.component';
+import { AnalyticsComponent } from './components/analytics/analytics.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'trips', pathMatch: 'full'},
+  { path: 'trips', component: TripsComponent},
+  { path: 'trips', component: AnalyticsComponent},
+
+
+];
