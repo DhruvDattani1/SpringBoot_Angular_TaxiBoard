aa40870a3c809f3335708cbcab7715b1ca0e6890
Dhruv Dattani <dhruvd22@gmail.com>
2026-03-25 16:47:51 -0400

    Phase 5: Login component created, along with registering the route in my routes collection

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
+import { Login } from './components/login/login'
 import { Routes } from '@angular/router';
 import { Trips } from './components/trips/trips';
 import { AnalyticsC } from './components/analytics/analytics';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'trips', pathMatch: 'full'},
+  { path: 'login', component: Login },
   { path: 'trips', component: Trips},
   { path: 'analytics', component: AnalyticsC},
 
---
+++ frontend/src/app/components/login/login.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Login {
+
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.form.invalid) return;
+
+    const { username, password } = this.form.value;
+
+    this.auth.login(username, password).subscribe({
+      next: () => this.router.navigate(['/trips']),
+      error: () => alert('Invalid credentials')
+    });
+  }
+
+  goToRegister() {
+    this.router.navigate(['/register']);
+  }
+}
