48aee0cb2f76b114dd6c3b954166d7f32b1fec9d
Dhruv Dattani <dhruvd22@gmail.com>
2026-03-26 11:41:44 -0400

    Phase 5: Implemented a shared application shell layout. Added persistent header and footer around the router outlet. And then used router.navigate to to wire everything via the component. Then just some conditional stuff to show links in the header only when user is logged in.

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,51 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('frontend');
+
+  constructor(
+    private router: Router,
+    private auth: AuthService
+  ) {}
+  isLoggedIn(): boolean {
+    return this.auth.isLoggedIn();
+  }
+
+  isLoginPage(): boolean {
+    return this.router.url === '/login';
+  }
+
+  isRegisterPage(): boolean {
+    return this.router.url === '/register';
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  goToRegister() {
+    this.router.navigate(['/register']);
+  }
+  goToTrips() {
+    this.router.navigate(['/trips'])
+  }
+
+  goToAnalytics() {
+    this.router.navigate(['/analytics']);
+  }
+
+  logout() {
+    this.auth.logout()
+  }
 }
